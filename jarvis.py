a5f51beff059b486ae8d1c1bc03f17e6b6eaa194
Siddhant Nirav Zaveri <zaverisid934@gmail.com>
2020-12-13 00:25:52 +0530

    music and joke feature addition

--- jarvis.py
+++ jarvis.py
@@ -9,13 +9,13 @@ import sys
 import random
 import smtplib
 import tkinter as tk
-import pywhatkit as kit
+import pywhatkit
+import pyjokes
 
 MASTER = 'Sid'
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
-# print(voices[1].id)
 engine.setProperty('voice', voices[0].id)
 
 client = wolframalpha.Client('K6Y537-G9KHELRWVH')
@@ -41,18 +41,6 @@ def wishMe():
     speak("How may i help You?")
 
 
-# def sendmsg():
-#     global num
-#     global text
-#     global time_h
-#     global time_m
-#     num = x.get()
-#     text = y.get()
-#     time_h = th.get()
-#     time_m = tm.get()
-#     root.destroy()
-#     kit.sendwhatmsg(num, text, time_h, time_m)
-
 
 def takeCommand():
     # it takes microphone input from the user and returns string output
@@ -60,7 +48,7 @@ def takeCommand():
     r = sr.Recognizer()
     with sr.Microphone() as source:
         print("Listening...")
-        r.pause_threshold = 2
+        r.pause_threshold = 2 #press ctrl on pause threshhold to increse decrese sensitivity 
         audio = r.listen(source)
 
         try:
@@ -92,7 +80,6 @@ if __name__ == "__main__":
             speak(results)
 
         elif 'open youtube' in query:
-            # webbrowser.open("youtube.com")
             url = "youtube.com"
             chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
             webbrowser.get(chrome_path).open(url)
@@ -122,11 +109,16 @@ if __name__ == "__main__":
             chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
             webbrowser.get(chrome_path).open(url)
 
-        elif 'play music' in query:
-            music_dir = 'C:\\Users\\siddhant\\Downloads\\music'
-            songs = os.listdir(music_dir)
-            print(songs)
-            os.startfile(os.path.join(music_dir, songs[0]))
+        elif 'play' in query:
+            song = query.replace('play', '')
+            speak('playing' + song)
+            pywhatkit.playonyt(song)
+
+              # elif 'play music' in query:
+        #     music_dir = 'C:\\Users\\siddhant\\Downloads\\music'
+        #     songs = os.listdir(music_dir)
+        #     print(songs)
+        #     os.startfile(os.path.join(music_dir, songs[0]))
 
         elif 'play perfect' in query:
             music_dir = 'C:\\Users\\siddhant\\Downloads\\music'
@@ -147,32 +139,8 @@ if __name__ == "__main__":
             speak(greet)
             # exit()
 
-        # elif 'send message' in query:
-        #     root = tk.Tk()
-        #     root.configure(bg="green")
-        #     x = tk.StringVar()
-        #     y = tk.StringVar()
-        #     th = tk.IntVar()
-        #     tm = tk.IntVar()
-
-        #     root.geometry("300x200")
-        #     root.title("Whatsapp using Python")
-        #     label1 = tk.Label(root, text="Enter your number:").grid(row=0, column=0)
-        #     label2 = tk.Label(root, text="Enter your text:").grid(row=1, column=0)
-        #     label3 = tk.Label(root, text="Enter time in hour:").grid(row=2, column=0)
-        #     label4 = tk.Label(root, text="Enter time in minute:").grid(row=3, column=0)
-
-        #     entry1 = tk.Entry(root, textvariable=x).grid(row=0, column=1)
-        #     entry2 = tk.Entry(root, textvariable=y).grid(row=1, column=1)
-        #     entry3 = tk.Entry(root, textvariable=th).grid(row=2, column=1)
-        #     entry4 = tk.Entry(root, textvariable=tm).grid(row=3, column=1)
-        #     button = tk.Button(root, text="Send", command=sendmsg).grid(row=4, column=0)
-
-        #     root.mainloop()
-
-        # elif 'I love you bro' in query:
-        #     love = "I love you 3000 Sir!"
-        #     speak(love)
+        elif 'joke' in query:
+            speak(pyjokes.get_joke())
 
         elif 'Whats up?' in query:
             wu = "Just doing my thing sir!"
@@ -182,7 +150,6 @@ if __name__ == "__main__":
             stop = "Hope u enjoyed my services. Goodbye!"
             speak(stop)
             exit()
-
 
         elif 'email' in query:
             speak('Who is the recipient')
@@ -199,7 +166,8 @@ if __name__ == "__main__":
 
                 mail.starttls()
 
-                mail.login('siddhant.zaveri@sakec.ac.in', 'enter your password')
+                mail.login('siddhant.zaveri@sakec.ac.in',
+                           'enter your password')
 
                 mail.sendmail('sid', 'zaverisid934@gmail.com', content)
 
@@ -224,7 +192,7 @@ if __name__ == "__main__":
                     speak(results)
 
                 except:
-                    results = wikipedia.summary(query, sentences= 2)
+                    results = wikipedia.summary(query, sentences=2)
                     speak('Just a minute sir...')
                     speak('Got It!')
                     print(results)
