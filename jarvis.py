c7922d538ec9972a2a09a9fdfb42927ea1863ebb
Siddhant Nirav Zaveri <zaverisid934@gmail.com>
2021-02-10 15:37:07 +0530

    minor update

--- jarvis.py
+++ jarvis.py
@@ -37,6 +37,7 @@ MASTER = 'Sid'
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
+print(voices)
 engine.setProperty('voice', voices[0].id)
 # engine.setProperty('rate', 180)
 
